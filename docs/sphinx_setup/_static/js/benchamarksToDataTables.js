c113aa347a9c5a27ac1a9b8f6aea85e2aebd63ad
Kopytko, AndrzejX <andrzejx.kopytko@intel.com>
2025-03-17 13:54:41 +0100

    fix

--- docs/sphinx_setup/_static/js/benchamarksToDataTables.js
+++ docs/sphinx_setup/_static/js/benchamarksToDataTables.js
@@ -13,29 +13,29 @@ $(document).ready(function () {
     $('body').css('overflow', 'hidden');
 
     fetch('../_static/benchmarks_files/data/' + fileName)
-      .then((response) => response.json())
-      .then((jsonData) => {
+      .then(response => response.json())
+      .then(jsonData => {
         fetch('../_static/benchmarks_files/graph-config.json')
-          .then((configResponse) => configResponse.json())
-          .then((appConfig) => {
-            renderModal(jsonData, appConfig);
+          .then(configResponse => configResponse.json())
+          .then(appConfig => {
+            renderModal(jsonData);
           });
       });
   }
 
-  function renderModal(graph, appConfig) {
+  function renderModal(graph) {
     var modalPath = '../_static/html/modaltable.html';
 
     fetch(modalPath)
-      .then((response) => response.text())
-      .then((html) => {
+      .then(response => response.text())
+      .then(html => {
         var modal = $('<div>').attr('id', 'graphModal').addClass('modal');
         var modalContent = $(html).attr('id', 'graphModalContent').addClass('modal-content');
 
         modal.append(modalContent);
         $('body').prepend(modal);
 
-        populateModalData(graph, appConfig);
+        populateModalData(graph);
 
         $('.modal-close').on('click', hideModal);
         modal.on('click', function (e) {
@@ -50,94 +50,116 @@ $(document).ready(function () {
       });
   }
 
-  function normalizeData(rawData) {
+  function normalizeData(rawData, metricType) {
     let allPrecisionKeys = new Set();
 
     let normalizedData = rawData.map(item => {
-      let throughputData = item.Parameters.throughput?.Precisions[0] || {};
-      let latencyData = item.Parameters.latency?.Precisions[0] || {};
+      let metricData = item.Parameters[metricType]?.Precisions[0] || {};
 
-      Object.keys(throughputData).forEach(key => allPrecisionKeys.add(key));
-      Object.keys(latencyData).forEach(key => allPrecisionKeys.add(key));
+      Object.keys(metricData).forEach(key => allPrecisionKeys.add(key));
 
       return {
         Platform: item.Platform,
         Model: item.Model,
-        PlatformType: item.PlatformType, 
-        throughput: throughputData,
-        latency: latencyData
+        PlatformType: item.PlatformType || "-",
+        metrics: metricData
       };
     });
 
     return { normalizedData, allPrecisionKeys: Array.from(allPrecisionKeys) };
   }
 
-  function populateModalData(graph, appConfig) {
-    let { normalizedData, allPrecisionKeys } = normalizeData(graph);
+  function populateModalData(graph) {
+    let metricType = 'throughput';
 
-    let tableHTML = `
-        <table id="benchmarkTable" class="display">
-            <thead>
-                <tr>
-                    <th style="width: 400px">Platform</th>
-                    <th style="width: 200px">Model</th>
-                    <th style="width: 200px">Platform Type</th>
-                    ${allPrecisionKeys.map(key => `<th>${key}</th>`).join('')}
-                </tr>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    `;
+    $('.chart-placeholder').html('<table id="benchmarkTable" class="display"></table>');
 
-    $('.chart-placeholder').html(tableHTML);
-    let tableBody = $('#benchmarkTable tbody');
-    normalizedData.forEach(row => {
-      let tableRow = `
+    function renderTable() {
+      let { normalizedData, allPrecisionKeys } = normalizeData(graph, metricType);
+      columnDefs = [{ "visible": false, "targets": [2] }]
+      let tableHTML = `
+        <thead>
+          <tr>
+            <th style="width: 400px">Platform</th>
+            <th style="width: 200px">Model</th>
+            <th style="width: 200px">Platform Type</th>
+            ${allPrecisionKeys.map(key => `<th>${key}</th>`).join('')}
+          </tr>
+        </thead>
+        <tbody>
+          ${normalizedData.map(row => `
             <tr>
-                <td>${row.Platform}</td>
-                <td>${row.Model}</td>
-                <td>${row.PlatformType}</td>
-                ${allPrecisionKeys.map(key => `<td>${row.throughput[key] || row.latency[key] || '-'}</td>`).join('')}
+              <td>${row.Platform}</td>
+              <td>${row.Model}</td>
+              <td>${row.PlatformType}</td>
+              ${allPrecisionKeys.map(key => `<td>${row.metrics[key] || '-'}</td>`).join('')}
             </tr>
-        `;
-      tableBody.append(tableRow);
-    });
+          `).join('')}
+        </tbody>
+      `;
 
-    var hidden = [2];
-    columnDefs = [{ "visible": false, "targets": JSON.parse(hidden) }];
-
-    $('#benchmarkTable').DataTable({
-      responsive: true,
-      "autoWidth": false,
-      language: {
-        buttons: {
-          colvisRestore: "Restore default selection"
-        }
-      },
-      lengthMenu: [
-        [10],
-        ['10 rows']
-      ],
-      "columnDefs": columnDefs,
-      layout: {
-        topStart: {
-          buttons: [
-            'pageLength',
-            {
-              extend: 'colvis',
-              postfixButtons: ['colvisRestore'],
-            },
-            {
-              extend: 'print',
-              text: 'Print pdf',
-              exportOptions: {
-                columns: ':visible'
-              }
-            }
-          ]
-        }
+      if ($.fn.DataTable.isDataTable('#benchmarkTable')) {
+        $('#benchmarkTable').DataTable().destroy();
+        $('#benchmarkTable').empty();
       }
-    });
+
+      $('#benchmarkTable').html(tableHTML);
+
+      $('#benchmarkTable').DataTable({
+        responsive: true,
+        "autoWidth": false,
+        language: {
+          buttons: {
+            colvisRestore: "Restore default view"
+          }
+        },
+        lengthMenu: [
+          [10],
+          ['10 rows']
+        ],
+        "columnDefs": columnDefs,
+        layout: {
+          topStart: {
+            buttons: [
+              'pageLength',
+              {
+                extend: 'colvis',
+                postfixButtons: ['colvisRestore'],
+              },
+              {
+                extend: 'print',
+                text: 'Print PDF',
+                exportOptions: {
+                  columns: ':visible'
+                }
+              },
+              {
+                extend: 'collection',
+                text: `Parameter: ${metricType}`,
+                autoClose: true,
+                buttons: [
+                  {
+                    text: 'Throughput',
+                    action: function () {
+                      metricType = 'throughput';
+                      renderTable();
+                    }
+                  },
+                  {
+                    text: 'Latency',
+                    action: function () {
+                      metricType = 'latency';
+                      renderTable();
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      });
+    }
+
+    renderTable();
   }
 });
